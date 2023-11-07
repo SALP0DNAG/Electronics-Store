395668a390b64b6347b4849afbd21482aacff25e
Salp0dnag <mereymaaa@gmail.com>
2023-11-07 11:27:34 +0300

    Add base.html, index.html

--- Grand_store/asgi.py
+++ Grand_store/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Electronics_store project.
+ASGI config for Grand_store project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Electronics_store.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Grand_store.settings')
 
 application = get_asgi_application()
