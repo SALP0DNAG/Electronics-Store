856d38a7315e5b855f5e72171a7f1cd3f87f21d7
Salp0dnag <mereymaaa@gmail.com>
2024-01-21 00:57:30 +0300

    add characteristics model

--- Store/models.py
+++ Store/models.py
@@ -28,3 +28,31 @@ class SpecialProducts(models.Model):
 
     def __str__(self):
         return f"SPECIAL {self.category}"
+
+
+class Characteristics(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    guarantee = models.PositiveIntegerField(default=None)
+    thread_diameter = models.DecimalField(default=None, max_digits=5, decimal_places=2)
+    material = models.CharField(max_length=30, default=None)
+    mounting_method = models.CharField(max_length=30, default=None)
+    purpose_mixer = models.CharField(max_length=50, default=None)
+    mixer_type = models.CharField(max_length=50, default=None)
+    type_liner = models.CharField(max_length=50, default=None)
+    width = models.PositiveIntegerField(default=None)
+    length = models.PositiveIntegerField(default=None)
+    weight = models.PositiveIntegerField(default=None)
+    height = models.PositiveIntegerField(default=None)
+    wear_resistance = models.CharField(max_length=20, default=None)
+    country = models.CharField(max_length=20, default=None)
+    basis = models.CharField(max_length=20, default=None)
+    color = models.CharField(max_length=20, default=None)
+    putty_type = models.CharField(max_length=20, default=None)
+    drying_time = models.PositiveIntegerField(default=None)
+    volume = models.PositiveIntegerField(default=None)
+    thickness = models.PositiveIntegerField(default=None)
+    glass_package = models.CharField(max_length=30, default=None)
+
+    def __str__(self):
+        return f"Характеристики для товара {self.product.name}"
+
